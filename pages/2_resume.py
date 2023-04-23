6c876b66d92788730d09b59d8bcbfa097bde92a9
rpj09 <singhripunjay09@gmail.com>
2023-04-23 10:32:47 +0530

    updated

--- pages/2_resume.py
+++ pages/2_resume.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import io
-import base64
-import fitz
 
 def css(filename):
     with open(filename) as f:
@@ -15,10 +12,6 @@ st.text('coming soon...')
 
 # Display PDF at the bottom
 pdf_url = "https://raw.githubusercontent.com/rpj09/Portfolio/master/images/rpjres.pdf"
-pdf_height = 800
 
-with fitz.open(pdf_url) as doc:
-    pdf_bytes = doc.get_pdf()
-    
-b64 = base64.b64encode(pdf_bytes)
-st.markdown(f'<embed src="data:application/pdf;base64,{b64.decode()}" width="100%" height="{pdf_height}" type="application/pdf">', unsafe_allow_html=True)
+
+st.pdf_viewer(pdf_url)
