fea9fbe4f9b0d65d89af2c8d53cd37c136f4db2e
rpj09 <singhripunjay09@gmail.com>
2023-04-23 10:38:31 +0530

    updated

--- pages/2_resume.py
+++ pages/2_resume.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_pdf_viewer import embed_pdf
+
 
 def css(filename):
     with open(filename) as f:
@@ -13,5 +13,5 @@ st.text('coming soon...')
 
 # Display PDF at the bottom
 pdf_url = "https://raw.githubusercontent.com/rpj09/Portfolio/master/images/rpjres.pdf"
-pdf_height = 800
-embed_pdf(pdf_url, height=pdf_height)
+pdf_height = 1000
+st.markdown(f'<iframe src="{pdf_url}" width="100%" height="{pdf_height}px"></iframe>', unsafe_allow_html=True)
