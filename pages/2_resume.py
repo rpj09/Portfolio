f84a9342150a6a2a7baf33a25a3f5a3139ef8e96
rpj09 <singhripunjay09@gmail.com>
2023-04-23 09:24:53 +0530

    updated

--- pages/2_resume.py
+++ pages/2_resume.py
@@ -9,3 +9,10 @@ css("style/style.css")
 st.title("Resume")
 
 st.text('coming soon...')
+
+
+
+# Display PDF at the bottom
+pdf_url = "https://github.com/rpj09/Portfolio/blob/master/images/Resume-Ripunjay-Singh%2Bnew.pdf?raw=true"
+pdf_height = 800
+st.markdown(f'<iframe src="{pdf_url}" width="100%" height="{pdf_height}" frameborder="0"></iframe>', unsafe_allow_html=True)
