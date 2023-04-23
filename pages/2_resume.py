59a4872fc39b7c5173ab0938746108bb40999531
rpj09 <singhripunjay09@gmail.com>
2023-04-23 10:26:29 +0530

    updated

--- pages/2_resume.py
+++ pages/2_resume.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import io
+import base64
 import fitz
 
 def css(filename):
@@ -9,20 +11,14 @@ css("style/style.css")
 
 st.title("Resume")
 
-st.text('coming soon....')
+st.text('coming soon...')
 
-
-st.text('Here is my Resume')
-
+# Display PDF at the bottom
 pdf_url = "https://raw.githubusercontent.com/rpj09/Portfolio/master/images/rpjres.pdf"
 pdf_height = 800
 
 with fitz.open(pdf_url) as doc:
-    pdf_data = doc.convert_to_html()
-st.markdown(pdf_data, unsafe_allow_html=True)
-# Display PDF at the bottom
-# pdf_url = "https://raw.githubusercontent.com/rpj09/Portfolio/master/images/rpjres.pdf"
-# pdf_height = 800
-# embed_pdf(pdf_url, height=pdf_height)
-
-#st.markdown(f'<iframe src="{pdf_url}" width="100%" height="{pdf_height}" frameborder="0"></iframe>', unsafe_allow_html=True)
+    pdf_bytes = doc.get_pdf()
+    
+b64 = base64.b64encode(pdf_bytes)
+st.markdown(f'<embed src="data:application/pdf;base64,{b64.decode()}" width="100%" height="{pdf_height}" type="application/pdf">', unsafe_allow_html=True)
