dc5c602f3d29791fe768e36c2dd9bac202297ce9
rpj09 <singhripunjay09@gmail.com>
2023-04-23 11:18:11 +0530

    updated

--- pages/2_resume.py
+++ pages/2_resume.py
@@ -21,7 +21,7 @@ st.text('here is my resume')
 # Display PDF at the bottom
 pdf_url = "https://raw.githubusercontent.com/rpj09/Portfolio/master/images/rpjres.pdf"
 response = requests.get(pdf_url)
-#pdf_height = 1000
+
 with open("dummy.pdf", "wb") as f:
     f.write(response.content)
 
@@ -30,5 +30,3 @@ with open("dummy.pdf", "rb") as f:
 
 pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
 st.markdown(pdf_display, unsafe_allow_html=True)
-#st.embed_pdf(pdf_url, width=700, height=900)
-#st.markdown(f'<iframe src="{pdf_url}" width="100%" height="{pdf_height}px"></iframe>', unsafe_allow_html=True)
