78f7723deb43fac38c118d6ec6f6bae3b9e46194
rpj09 <singhripunjay09@gmail.com>
2023-04-23 11:35:38 +0530

    updated

--- pages/2_resume.py
+++ pages/2_resume.py
@@ -1,4 +1,5 @@
 import streamlit as st
+impot requests
 from PIL import Image
 
 
@@ -12,12 +13,18 @@ def css(filename):
 css("style/style.css")
 
 st.title("Resume")
-
-st.text('coming soon...')
-
-st.text('here is my resume')
-
+st.markdown("## Ripunjay Singh")
 res1 = Image.open("images/res1.jpg")
 res2 = Image.open("images/res2.jpg")
 st.image(res1, use_column_width=True)
 st.image(res2,use_column_width=True)
+
+# PDF file URL
+pdf_url = "https://github.com/rpj09/Portfolio/blob/master/images/rpjres.pdf?raw=true"
+
+# Download button
+if st.button("Download Resume"):
+    response = requests.get(pdf_url)
+    with open("rpjres.pdf", "wb") as f:
+        f.write(response.content)
+    st.success("Download complete!")
