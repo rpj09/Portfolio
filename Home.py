dff29b2ac1525ca54591d5e93266a1a7c38bf4e3
rpj09 <singhripunjay09@gmail.com>
2023-05-26 01:35:12 +0530

    updated bg color

--- Home.py
+++ Home.py
@@ -61,7 +61,7 @@ with st.container():
     st.title("A passionate learner and Python Developer")
     st.write("I am passionate about learning different technologies and finding ways to automate stuff using python")
     st.write("[Explore more about my work>](https://github.com/rpj09)")
-    with right_column:
+    with right:
         st_lottie(lottie_gif, height=100, key="cod")
 
     left_column,right_column = st.columns(2)
