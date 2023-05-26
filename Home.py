ef69626736e1af1fef6d0f2e0cd1d06289389e9a
rpj09 <singhripunjay09@gmail.com>
2023-05-27 01:29:16 +0530

    updated bg color

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ pg_bg_gradient = """
 
 <style>
 [class="main css-uf99v8 egzxvld5"]{
-background-image: linear-gradient(to right top, #f4bedc, #e7b8df, #d7b2e3, #c3aee6, #acabe8, #979edb, #8192cd, #6c86bf, #596ea2, #465786, #33416b, #212c51);
+background-image: linear-gradient(180deg, rgba(11,16,19,1) 0%, rgba(38,4,54,1) 48%, rgba(9,16,21,1) 100%);
 }
 </style>
 
