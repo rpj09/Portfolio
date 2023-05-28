18e786738826fc97ccb19a4f65eea7db5500ade7
rpj09 <singhripunjay09@gmail.com>
2023-05-28 10:07:51 +0530

    updated bg color

--- Home.py
+++ Home.py
@@ -43,7 +43,7 @@ card = """
 
 tansbuttonbg1 = """
 <style>
-[class="st-bv st-bw st-b6 st-b5 st-ar st-as st-bx st-by st-bz st-c0 st-c1 st-c2 st-c3 st-c4 st-c5 st-c6 st-c7 st-c8 st-c9 st-ca st-cb st-cu st-cd st-ce st-b1 st-cf st-cg st-ch st-ci st-cj st-ck st-cl st-cm st-ae st-cn st-ag st-ah st-ai st-aj st-co st-cp st-cq st-cr st-cs st-ct"]{
+[class="st-c7"]{
 background: transparent;
 }
 </style>
